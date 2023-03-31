7a61b0b2b5501e3499c319eb1568e937fc7335ac
LucasNahuel <munozlucas96@gmail.com>
2023-03-31 01:17:03 -0300

    third step completed and other fixes

--- src/app/third-step/third-step.component.ts
+++ src/app/third-step/third-step.component.ts
@@ -10,6 +10,27 @@ export class ThirdStepComponent implements OnInit {
 
   formService : FormInfoService | undefined;
 
+  addons = [
+    {
+      name : "Online services",
+      description: "Access to multiplayer games",
+      selected: false,
+      monthlyPrice: 1
+    },
+    {
+      name : "Larger Storage",
+      description: "Extra 1TB of cloud save",
+      selected: false,
+      monthlyPrice: 2
+    },
+    {
+      name : "Customizable profile",
+      description: "Custom theme on your profile",
+      selected: false,
+      monthlyPrice: 2
+    }
+  ]
+
   constructor(formService : FormInfoService) {
     this.formService = formService;
   }
@@ -24,6 +45,10 @@ export class ThirdStepComponent implements OnInit {
 
   nextStep(){
     this.formService?.changeToStep(4);
+  }
+
+  toggleAddon(index : number){
+    this.addons[index].selected = !this.addons[index].selected;
   }
 
 }
